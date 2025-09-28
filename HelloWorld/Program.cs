1f9d790b7d09c64690133a6ed20d11c71a7ffaa4
Daniel Henao Carvajal <danielhenao1123010@correo.itm.edu.co>
2025-09-28 17:05:43 -0500

    Add new code for Age Calculation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,16 +1,39 @@
 ﻿namespace HelloWorld;
 
-internal class NewBaseType
+class Program
 {
-    // Clase base vacía
+    static void Main()
+    {
+        DateOnly dateConverted;
+        string nameInput;
+        string birthdayInput;
+        Console.WriteLine("Hola Bienvenido a el calculador de años");
+        Console.WriteLine("Escribe tu nombre:");
+        nameInput = Console.ReadLine();
+        Console.WriteLine($"Un gusto conocerte {nameInput}");
+        Console.WriteLine("Escribe tu fecha de nacimiento en formato dd/mm/yy:");
+        birthdayInput = Console.ReadLine();
+        bool isDateValid = DateOnly.TryParse(birthdayInput, out dateConverted);
+        if (!isDateValid)
+        {
+            Console.WriteLine($"La fecha de nacimiento no es valida, usted ingreso este dato erroneamente {birthdayInput} ");
+            return;
+        }
+        var person = new Person
+        {
+            Name = nameInput,
+            Birthday = dateConverted,
+            Age = DateTime.Now.Year - dateConverted.Year
+        };
+        Console.WriteLine($"Tu nombre es: {person.Name}, tu fecha de nacimiento es: {person.Birthday}, y tu edad es: {person.Age} años");
+        Console.WriteLine($"Gracias por usar el programa {person.Name}, hasta pronto!");
+        Console.ReadLine();
+    }
 }
 
-class Program : NewBaseType
+public class Person
 {
-    static void Main() {
-        var Name = "Daniel Henao";
-        Console.WriteLine($"Hello, World! {Name}");
-        Console.WriteLine($"Thanks for coming {Name}");
-        Console.WriteLine();
-    }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public DateOnly Birthday { get; set; }
 }
